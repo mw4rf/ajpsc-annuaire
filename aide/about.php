f1319d03fd281eac2db68d731c969fd0aa060c7a
mw4rf <mw4rf@sent.at>
2013-02-15 00:27:34 +0100

    about page

--- aide/about.php
+++ aide/about.php
@@ -1,58 +1,66 @@
-<div class="aide_chapitre">
-	Droits d'auteur
+<div class="container">
+
+    <div class="aide_chapitre">
+    	Droits d'auteur
+    </div>
+
+    <ul class="aide_section_light">
+    	<li />Auteur: Guillaume Florimond (<a href="mailto:gflorimond at gmail dot com">e-mail</a>)
+    	<li />Contrat de licence: <a href="http://creativecommons.org/licenses/by-sa/2.0/fr/legalcode">Creative Commons Paternité-ShareAlike 2.0 France</a>
+    	<li />Licence 2006-<?php echo date("Y");?> pour AJPSC.Com
+    	<li />Code source disponible: <a href="https://bitbucket.org/mw4rf/ajpsc-annuaire">Bitbucket Git</a>
+    </ul>
+
+    <p class="aide_chapitre">
+    	Version AJPSC.Com
+    </p>
+
+    <ul class="aide_section_light">
+    	<li />Version installée: 2.0
+    	<li />Version en date: du XX XXXX XXXX
+    	<li />Développement initié: le 7 décembre 2006
+    	<li />Date de lancement: 21 février 2007 (version 1.0)
+    	<li />Contenu: <a href="divers/tables.sql">schema MySQL</a>
+    </ul>
+
+    <p class="aide_chapitre">
+    	Produits embarqués
+    </p>
+
+    <ul class="aide_section_light">
+    	<li />JavaScript <i>Sweet Titles</i>, <a href="http://creativecommons.org/licenses/by-sa/2.5/">Creative Commons 2005</a>, Auteur: Dustin Diaz.
+    	<li />JavaScript <i>Menu de redirection</i>, Adobe (Macromedia) DreamWeaver 8.0
+    	<li />(version 1.x)Thèmes AJPSC et Valhalla: Developer Icons by Matt Ball ; icône Acrobat: Copyright Adobe Systems Incorporated.
+    	<li />FPDF 1.53 par Olivier Plathey (licence GPL, produit libre de droits).
+        <li />(version 2.x)<a href="http://jquery.com/">jquery</a>
+        <li />(version 2.x)<a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a>
+    </ul>
+
+    <p class="aide_chapitre">
+    	Technologies utilisées
+    </p>
+
+    <ul class="aide_section_light">
+    	<li />(version 1.x)XHTML 1.0 transitional / (version 2.x) HTML 5
+        <li />Exportation: XML, XLS (Excel), CSV
+    	<li />(version 1.x)PHP 4.0+ / (version 2.x)PHP 5.2+
+    	<li />MySQL 5.0+
+    	<li />JavaScript
+    </ul>
+
+    <p class="aide_chapitre">
+    	Compatibilité
+    </p>
+
+    <ul class="aide_section_light">
+    	<li />Microsoft Internet Explorer 7/8/9/10+ PC: compatible - compatibilité assurée
+    	<li />Microsoft Internet Explorer 6 PC: compatible - compatibilité non assurée
+    	<li />Firefox PC/Mac/Linux (Gecko): compatible - compatibilité assurée
+    	<li />Safari (Webkit): compatible - compatibilité assurée
+        <li />Chrome: compatible - compatibilité assurée
+    	<li />Konqueror (KHTML): compatible - compatibilité non assurée
+    	<li />Opera: compatible - compatibilité non assurée
+    	<li />Autres (KHTML): non compatible - compatibilité non assurée
+    </ul>
+
 </div>
-
-<ul class="aide_section_light">
-	<li />Auteur: Guillaume Florimond (<a href="mailto:gflorimond at gmail dot com">e-mail</a>)
-	<li />Contrat de licence: <a href="http://creativecommons.org/licenses/by-sa/2.0/fr/legalcode">Creative Commons Paternité-ShareAlike 2.0 France</a>
-	<li />Licence 2006-<?php echo date("Y");?> pour AJPSC.Com
-	<li />Code source disponible: <a href="http://downloads.valhalla.fr/">serveurs Valhalla.fr</a>
-</ul>
-
-<p class="aide_chapitre">
-	Version AJPSC.Com
-</p>
-
-<ul class="aide_section_light">
-	<li />Version installée: 1.3.1
-	<li />Version en date: du 16 avril 2007
-	<li />Développement initié: le 7 décembre 2006
-	<li />Date de lancement: 21 février 2007 (version 1.0)
-	<li />Contenu: <a href="">dump MySQL (vide)</a>
-</ul>
-
-<p class="aide_chapitre">
-	Produits embarqués
-</p>
-
-<ul class="aide_section_light">
-	<li />JavaScript <i>Sweet Titles</i>, <a href="http://creativecommons.org/licenses/by-sa/2.5/">Creative Commons 2005</a>, Auteur: Dustin Diaz.
-	<li />JavaScript <i>Menu de redirection</i>, Adobe (Macromedia) DreamWeaver 8.0
-	<li />Thèmes AJPSC et Valhalla: Developer Icons by Matt Ball ; icône Acrobat: Copyright Adobe Systems Incorporated.
-	<li />FPDF 1.53 par Olivier Plathey (licence GPL, produit libre de droits).
-</ul>
-
-<p class="aide_chapitre">
-	Technologies utilisées
-</p>
-
-<ul class="aide_section_light">
-	<li />XHTML 1.0 (transitional) et XML (exportation)
-	<li />PHP 4.0+
-	<li />MySQL 5.0+
-	<li />JavaScript
-</ul>
-
-<p class="aide_chapitre">
-	Compatibilité
-</p>
-
-<ul class="aide_section_light">
-	<li />Microsoft Internet Explorer 7 PC: compatible - compatibilité assurée
-	<li />Microsoft Internet Explorer 6 PC: compatible - compatibilité non assurée
-	<li />Firefox 1.5 et 2.0+ PC/Mac/Linux (Gecko): compatible - compatibilité assurée
-	<li />Safari 2.0+ / Omniweb 5.5+ (Webkit): compatible - compatibilité assurée
-	<li />Konqueror (KHTML): compatible - compatibilité non assurée
-	<li />Opera: compatible - compatibilité non assurée
-	<li />Autres (KHTML): non compatible - compatibilité non assurée
-</ul>
