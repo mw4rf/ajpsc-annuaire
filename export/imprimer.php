7cc1f8fcf8d3decae4da8876245d812645be43d2
mw4rf <mw4rf@sent.at>
2013-02-15 22:08:39 +0100

    photo in print

--- export/imprimer.php
+++ export/imprimer.php
@@ -124,7 +124,13 @@ if($imgW == 0) $imgW = 1;
 $thumb_width = $_config['images_largeur_fiches'];
 $thumb_height = round($imgH / $imgW * $thumb_width);
 
-$photo = "<img height=\"$thumb_height\" width=\"$thumb_width\" src=\"../includes/photo.php?id=".$data['id']."\">";
+// Storage: DB / FS ?
+if($_config['photos_storage'] == "DB")
+    $src = "../includes/photo.php?id=$voir_id";
+else
+    $src = $_config['data_folder']."/photos/".$data2['user_id'].'.'.$data2['extension'];
+
+$photo = "<img height=\"$thumb_height\" width=\"$thumb_width\" src=\"$src\">";
 
 ?>
 
